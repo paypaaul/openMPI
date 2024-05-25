23b9e1c44ce4eb9f595fe28b07a19460144dfbbb
Paolo Vezzini <paul@MBP-di-Paolo-3.home-life.hub>
2024-05-25 15:55:23 +0200

    debug

---
+++ serial/rabin_karp.h
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <limits.h>
+#include "types.h"
+#include <time.h>
+#define PRIME_NUMBER 101
+
+uint64_t hash_code(char *str, int len) {
+    unsigned long long hash = 0;
+    for (int i = 0; i < len; i++) {
+        hash = (hash * PRIME_NUMBER + str[i]) % INT32_MAX;
+    }
+    return hash;
+}
+
+void rabin_karp(status_t *status, char *pattern, char *str) {
+    uint64_t pat_hash;
+    uint64_t str_hash;
+    int str_len = strlen(str);
+    int pat_len = strlen(pattern);
+    int i, j = 0;
+
+    if(strlen(pattern)>strlen(str)){
+        status->current_err = UNCOMPATIBLE_SIZES;
+        puts("pattern lenght is greater than source one");
+        exit(-1);
+    }
+
+    pat_hash = hash_code(pattern, pat_len);
+    
+    for(i = 0; i <= str_len-pat_len;i++) {
+        str_hash = hash_code(str+i, pat_len);
+        if(pat_hash == str_hash){
+            // pattern detected, checking collision...
+            for (j = 0; j < pat_len; j++){
+                if(pattern[j] != str[i+j]){
+                    status->current_err = HASH_COLLISION;
+                    // collision detected and avoided
+                    break;
+                }
+            }
+            if(status->current_err != HASH_COLLISION) {
+
+                // match found
+                status->match_indexes[status->freq] = i;
+                status->freq = status->freq + 1;
+            }
+        }
+    }
+    if(status->freq==0){
+        status->current_err = NOT_FOUND;
+        // pattern not found in the string
+    }
+}
